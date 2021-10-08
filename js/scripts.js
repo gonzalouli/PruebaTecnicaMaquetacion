7a2b8f06403ce1ad6347fe717a1590cbe10193f2
GonzaloUli <gonzalo.ulibarri@gmail.com>
2021-10-08 22:09:26 +0200

    validacion de formulario sin uso de html

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,43 @@ const now = moment().format('yyyy-MM-DD')
 document.addEventListener('DOMContentLoaded', ()=>{
 
     const fechaMin = document.querySelector('#date-imput');
+    const formulario = document.querySelector('#form')
+    const enviar= document.querySelector('#enviar')
 
     fechaMin.setAttribute('min', now);
 
-    const enviar= document.querySelector('#enviar')
 
-    enviar.addEventListener('submit', ()=>{
+    formulario.addEventListener('submit', (e)=>{
+        e.preventDefault();
 
-        const checked = document.querySelectorAll('#hour')
+        const hour = document.querySelectorAll('#hour')
+        const nombreimput = document.querySelectorAll('#name-imput').value
+        const apellidoimput = document.querySelectorAll('#apellido-imput').value
+        const emailimpuit = document.querySelectorAll('#email-imput').value
+        const fechaimput = document.querySelectorAll('#date-imput').value
+
+        if(nombreimput==='' || apellidoimput==='' || (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(emailimpuit)) || fechaimput<now  )
+        {
+            mostrarAlerta("Por favor, introduce los datos correctamente","error")
+            return
+        }
 
         let error=true;
-        checked.forEach( (item,error )=>{
-            if(item.checked==true){
+        hour.forEach( (item )=>{
+            if(item.checked===true){
                 error=false
             }
         })
-        if(error)
+        if(error){
             mostrarAlerta("Seleccione una hora por favor","error")
-        else
+            return;
+        }else
             mostrarAlerta("Mensaje enviado, contactaremos con usted proximamente","success")
 
     } )
+
+
+
 
     
 
@@ -33,26 +49,29 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 function mostrarAlerta(mensaje,tipo){
 
-    const alerta = document.createElement('div')
 
-    alerta.innerHTML = `
-    <span class="alerta">${mensaje}</span>
-    `
-    if(tipo=="error")
-        alerta.classList.add('alert', 'alert-danger')  
-    else
-        alerta.classList.add('alert', 'alert-success')  
+    const hayAlerta = document.querySelector('.alert')
+
+    if(!hayAlerta){
+        const alerta = document.createElement('div')
+        alerta.innerHTML = `
+        <span class="alerta">${mensaje}</span>
+        `
+        if(tipo=="error")
+            alerta.classList.add('alert', 'alert-danger','mt-3')  
+        else
+            alerta.classList.add('alert', 'alert-success','mt-3')  
+        
+        const formulario = document.querySelector('#idform')
     
-    const formulario = document.querySelector('#idform')
-
-    formulario.appendChild(alerta)
-
-    setTimeout( ()=>{
-        alerta.remove()
-    },3000)
+        formulario.appendChild(alerta)
     
+        setTimeout( ()=>{
+            alerta.remove()
+        },3000)
 
-
+    }
 
 
 }
+
